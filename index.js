cb9b85806d0f86922f15809273611bb3c07a87c1
DeepaThangaraju <deepat2405@gmail.com>
2021-12-14 16:29:54 +0530

    cors

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
 import { studentrouter } from "./routes/student.js";
 import { mentorrouter } from "./routes/mentor.js";
+import cors from 'cors';
 import { getstudentbyparams, getstudentbyid, deletestudentbyid, editbyid, createstudent } from "./studentmethod.js";
 import { getmentorbyquery, getmentorbyid, deletementorbyid, editmentorbyid, creatementor } from "./mentormethod.js";
 dotenv.config();
@@ -11,7 +12,9 @@ console.log(process.env);
 const app = express();
 
 const PORT = process.env.PORT;
+app.use(cors());
 app.use(express.json());
+
 const mentor = [
   {"id":"100","mentor_name":"deepa","students":["mithran","maha","abishek","Iniyan"]},
   {"id":"101","mentor_name":"deepika","students":["mithun","mathi","abinesh","aravind"]},
