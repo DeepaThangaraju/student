f5ec1e8676c1df1f08e1b22b43c30e8b7b5dbb40
DeepaThangaraju <deepat2405@gmail.com>
2021-12-16 13:50:26 +0530

    mongo object id

--- studentmethod.js
+++ studentmethod.js
@@ -1,4 +1,5 @@
 import { client } from "./index.js";
+import {ObjectId} from "mongodb";
 //it contains query that retrive  the data from mongodb
  async function createstudent(data) {
     return await client
@@ -22,7 +23,7 @@ import { client } from "./index.js";
     return await client
         .db("studentmentor")
         .collection("student")
-        .findOne({ id: id });
+        .findOne({ _id: ObjectId(id) });
 }
  async function getstudentbyparams(filter) {
     return await client.db("studentmentor").collection("student").find(filter).toArray();
